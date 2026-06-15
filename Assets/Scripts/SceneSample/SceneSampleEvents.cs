91867537ae1a7a86e56d7ba5d8f152ad4214890d
Myan <215093362+MyanMario@users.noreply.github.com>
2026-06-14 21:29:23 -0400

    working on Starting Dialogue

--- Assets/Scripts/SceneSample/SceneSampleEvents.cs
+++ Assets/Scripts/SceneSample/SceneSampleEvents.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
-using Systems.Collections;
-//using Systems.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
+using Yarn.Unity;
 
 public class SceneSampleEvents : MonoBehaviour
 {
     public GameObject fadeScreenIn;
+    public GameObject fadeScreenOut;
+
     public GameObject charGirl;
+    [SerializeField] internal YarnProject yarnProject;
+    //[YarnNode(nameof(yarnProject))]
+    public string startNode = "Start";
+    public DialogueRunner DialogueRunner;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +20,7 @@ public class SceneSampleEvents : MonoBehaviour
         PlayerPrefs.SetInt("LoadState", 1);
         StartCoroutine(EventStarter());
     }
-
+    
     IEnumerator EventStarter()
     {
         yield return new WaitForSeconds(2);
@@ -21,7 +28,11 @@ public class SceneSampleEvents : MonoBehaviour
         charGirl.SetActive(true);
         yield return new WaitForSeconds(2);
         //this is where the text box would start
+        DialogueRunner.StartDialogue(startNode);
         yield return new WaitForSeconds(2);
 
+        fadeScreenOut.SetActive(true);
+        yield return new WaitForSeconds(2);
     }
+    
 }
