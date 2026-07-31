bbd31643c4492f8a7bbe34d0fc6faedf8af28fad
Alyssa Foote <alyssa.foote.22@gmail.com>
2026-07-30 22:19:51 -0700

    Adjust background art and move it into sprites folder
    I made sure the background art is consistent in the title background, settings background, and the modal bg
    
    I moved the background folder into the "environments" folder under sprites since it is technically artwork
    
    I set up the script "GentleDrift" with cached RectTransform and start position

--- Assets/Scripts/GentleDrift.cs
+++ Assets/Scripts/GentleDrift.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class GentleDrift : MonoBehaviour
 {
+
+  RectTransform rt;
+Vector2 startPos;
     void Start()
     {
-
+        rt = GetComponent<RectTransform>();
+        startPos = rt.anchoredPosition;
     }
 
     void Update()
