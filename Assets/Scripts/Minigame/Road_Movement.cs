a56af675c8e22a51ace108d6c23cd5999754124f
tristynl <tristyn-lai@outlook.com>
2026-09-05 15:10:09 -0700

    Minigame Road Fix

--- Assets/Scripts/Minigame/Road_Movement.cs
+++ Assets/Scripts/Minigame/Road_Movement.cs
@@ -7,28 +7,37 @@ public class Road_Movement : MonoBehaviour
     public Renderer MeshRenderer;
     public float Speed = 1f;
     public float Max_Speed = 2f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Material roadMaterial;
+
     void Start()
     {
+        if (MeshRenderer != null)
+        {
+            roadMaterial = MeshRenderer.material;
+        }
         StartCoroutine(IncreaseSpeed());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MeshRenderer.material.mainTextureOffset += new Vector2(0, Speed * Time.deltaTime);
+        if (roadMaterial != null)
+        {
+            roadMaterial.mainTextureOffset += new Vector2(0, Speed * Time.deltaTime);
+        }
     }
 
-    public void ResetSpeed() {
+    public void ResetSpeed() 
+    {
         Speed = 1f;
         StopAllCoroutines();
         StartCoroutine(IncreaseSpeed());
     }
 
-    IEnumerator IncreaseSpeed() {
+    IEnumerator IncreaseSpeed() 
+    {
         while (true) {
             yield return new WaitForSeconds(5);
-            if (Max_Speed > Speed ) {
+            if (Max_Speed > Speed) {
                 Speed += 0.1f;
             }
             print("Road Speed: " + Speed);
