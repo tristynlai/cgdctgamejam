1c1732489c48b3fde6804f6e929bfb77fce8bb45
tristynl <tristyn-lai@outlook.com>
2026-09-23 23:49:12 -0700

    Added Confirmation Button for Save Button

---
+++ Assets/Scripts/Dialogue/SaveModeController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class SaveModeController : MonoBehaviour
+{
+    [SerializeField] private GameObject confirmationImage;
+    [SerializeField] private float displayDuration = 2f;
+
+    private Coroutine hideCoroutine;
+
+    private void Awake()
+    {
+        if (confirmationImage != null)
+        {
+            confirmationImage.SetActive(false);
+        }
+    }
+
+    public void ShowConfirmation()
+    {
+        if (confirmationImage == null) return;
+
+        if (hideCoroutine != null)
+        {
+            StopCoroutine(hideCoroutine);
+        }
+
+        confirmationImage.SetActive(true);
+        hideCoroutine = StartCoroutine(HideAfterDelay());
+    }
+
+    private IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(displayDuration);
+        confirmationImage.SetActive(false);
+        hideCoroutine = null;
+    }
+}
